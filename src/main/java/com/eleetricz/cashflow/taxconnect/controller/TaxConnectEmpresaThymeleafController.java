9b68dbaa4933e3c78401987ac0886ba746ea4c3e
EleeTricz <eriky237@gmail.com>
2026-04-02 09:33:25 -0300

    feat: Adiciona rota para integracaoTaxConnect passando EmpresaId como parametro e substitui botão para integração na pagina empresa-visualizar.

--- src/main/java/com/eleetricz/cashflow/taxconnect/controller/TaxConnectEmpresaThymeleafController.java
+++ src/main/java/com/eleetricz/cashflow/taxconnect/controller/TaxConnectEmpresaThymeleafController.java
@@ -1,42 +1,52 @@
 package com.eleetricz.cashflow.taxconnect.controller;
 
+import com.eleetricz.cashflow.entity.Empresa;
 import com.eleetricz.cashflow.service.EmpresaService;
 import com.eleetricz.cashflow.taxconnect.dto.ImportacaoTaxConnectFormDTO;
 import com.eleetricz.cashflow.taxconnect.service.ImportacaoTaxConnectService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@RequestMapping("/integracoes/taxconnect")
-public class TaxConnectThymeleafController {
+@RequestMapping("/integracoes/taxconnect/empresa")
+@RequiredArgsConstructor
+public class TaxConnectEmpresaThymeleafController {
 
     private final EmpresaService empresaService;
     private final ImportacaoTaxConnectService importacaoService;
 
-    public TaxConnectThymeleafController(EmpresaService empresaService, ImportacaoTaxConnectService importacaoService) {
-        this.empresaService = empresaService;
-        this.importacaoService = importacaoService;
+    @GetMapping("/{empresaId}")
+    public String telaImportacao(@PathVariable Long empresaId, Model model) {
+        Empresa empresa = empresaService.buscarPorId(empresaId);
+        if (empresa == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        ImportacaoTaxConnectFormDTO form = new ImportacaoTaxConnectFormDTO();
+        form.setEmpresaId(empresaId);
+
+        model.addAttribute("empresa", empresa);
+        model.addAttribute("form", form);
+
+        return "integracoes/taxconnect-importacao-empresa";
     }
 
-    @GetMapping
-    public String telaImportacao(Model model) {
-
-        model.addAttribute("empresas", empresaService.listarTodas());
-        model.addAttribute("form", new ImportacaoTaxConnectFormDTO());
-
-        return "integracoes/taxconnect-importacao";
-    }
-
-    @PostMapping("/importar")
+    @PostMapping("/{empresaId}/importar")
     public String importar(
+            @PathVariable Long empresaId,
             @ModelAttribute("form") ImportacaoTaxConnectFormDTO form,
             RedirectAttributes redirectAttributes) {
+
+        form.setEmpresaId(empresaId);
 
         try {
             importacaoService.importar(form);
@@ -53,6 +63,6 @@ public class TaxConnectThymeleafController {
             );
         }
 
-        return "redirect:/integracoes/taxconnect";
+        return "redirect:/integracoes/taxconnect/empresa/" + empresaId;
     }
 }
